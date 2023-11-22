b9c9519f18a15887361b49c813fb6d47ffd10bf3
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2023-11-22 19:27:49 +0200

    some format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home"
+import About from './components/About/About'
 
 function App() {
   return (
     <>
     <Navbar />
-<Home/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
     <Footer/>
     </>
   )
