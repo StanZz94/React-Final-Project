57c83c1a11988f4223945460b2242615744a3453
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2023-11-26 18:32:58 +0200

    update fether

--- client/services/postService.js
+++ client/services/postService.js
@@ -1,22 +1,16 @@
-import { request } from "./request";
+import * as request  from "./request";
 
 const baseUrl = 'http://127.0.0.1:3030/jsonstore/posts';
 
 export const getAll = async () =>{
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 }
 
 export const create = async (postData) => {
-    const res = await fetch(baseUrl, {
-        method: 'POST',
-        headers:{
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(postData),
-    })
-    const result = await res.json();
+
+    const result = await request.post(baseUrl, postData);
 
     return result;
 }
