6ff57021c435fffff411e5c0c3c446b3e4395c74
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2023-12-18 23:54:39 +0200

    format

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,27 +5,22 @@ import Path from "../../paths";
 
 export default function Navbar() {
   const { isAuthenticated, name} = useContext(AuthContext);
-  const [isActive, setIsActive] = useState(false);
-
-  const activeHandler = (e) => {
- 
-  }
 
   return (
       <nav className="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-5">
         <div className="navbar-nav mx-auto py-0">
-          <Link onClick={activeHandler} to={Path.Home} className="nav-item nav-link tab">
+          <Link to={Path.Home} className="nav-item nav-link tab">
             Home
           </Link>
-          <Link onClick={activeHandler} to={Path.About} className="nav-item nav-link tab">
+          <Link to={Path.About} className="nav-item nav-link tab">
             About us
           </Link>
-          <Link onClick={activeHandler} to={Path.Posts} className="nav-item nav-link tab">
+          <Link to={Path.Posts} className="nav-item nav-link tab">
             Blog
           </Link>
           {isAuthenticated && (
             <>
-              <Link onClick={activeHandler} to={Path.Create} className="nav-item nav-link tab">
+              <Link to={Path.Create} className="nav-item nav-link tab">
                 Create Post
               </Link>
               <Link  to={Path.Logout} className="nav-item nav-link">
